d22c531ca1748b307977d70a14c895916c581229
jpm-dias <jpm.dias@campus.fct.unl.pt>
2017-08-30 12:11:18 +0100

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,34 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        console.log('Unable to connect to MongoDB server');
+        return;
+    }
+    console.log('Connected to MongoDB server');
+
+    // Delete Many
+    // db.collection('Todos').deleteMany({
+    //     text: 'Eat lunch'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // Delete One
+    // db.collection('Todos').deleteOne({
+    //     text: 'Eat lunch'
+    // }).then((result) => {
+    //     console.log(result);
+    // });
+
+    // Find One And Delete
+    db.collection('Todos').findOneAndDelete({
+        completed: false
+    }).then((result) => {
+        console.log(result);
+    });
+
+    //db.close();
+});
